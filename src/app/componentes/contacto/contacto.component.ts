9fddd8220634fd585307c6fe9e3e419afa79f45d
GaboHerreraZ <herzar_620@hotmail.com>
2021-01-04 19:15:39 +0100

    Se agregan mejoras a responsive y pantalla de contacto

--- src/app/componentes/contacto/contacto.component.ts
+++ src/app/componentes/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, AbstractControl, FormGroup, Validators, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoPrincipalComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('form') form: NgForm;
+  formContacto: FormGroup;
+  get nombre(): AbstractControl { return this.formContacto.get('nombre'); }
+  get telefono(): AbstractControl { return this.formContacto.get('telefono'); }
+  get mensaje(): AbstractControl { return this.formContacto.get('mensaje'); }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.formContacto = this.createFormContacto();
   }
 
+  enviarContactor(): void {
+    console.log(this.formContacto.value);
+    this.form.resetForm();
+    console.log(this.formContacto);
+  }
+
+  createFormContacto(): FormGroup {
+    return  this.fb.group({
+      nombre: ['', Validators.required],
+      telefono: ['', Validators.required],
+      email: [''],
+      mensaje: ['', Validators.required]
+    });
+  }
 }
--- src/app/componentes/contacto/contacto.module.ts
+++ src/app/componentes/contacto/contacto.module.ts
@@ -4,15 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ContactoRoutingModule } from './contacto-routing.module';
 import { ContactoPrincipalComponent } from './contacto.component';
 import { AngularMaterialModule } from 'src/app/shared/module/angular-material/angular-material.module';
+import { ComunModule } from 'src/app/shared/module/comun/comun.module';
 
 
 @NgModule({
   declarations: [ContactoPrincipalComponent],
   imports: [
     CommonModule,
+    ComunModule,
     ContactoRoutingModule,
     AngularMaterialModule
-
   ]
 })
 export class ContactoModule { }
